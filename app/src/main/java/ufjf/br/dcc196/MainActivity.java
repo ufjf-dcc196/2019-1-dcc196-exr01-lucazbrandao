f48b4cef7d8a72ebd2d4717b6bfb576838d13f61
lucazbrandao <lucazbrandao@gmail.com>
2019-03-26 14:32:08 -0300

    Implemented code to control components

--- app/src/main/java/ufjf/br/dcc196/MainActivity.java
+++ app/src/main/java/ufjf/br/dcc196/MainActivity.java
@@ -2,12 +2,39 @@ package ufjf.br.dcc196;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGetFullName;
+    TextView txtName;
+    TextView txtLastName;
+    TextView txtFullName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnGetFullName = findViewById(R.id.btn_getfullname);
+        txtLastName = findViewById(R.id.txt_sobrenome);
+        txtName = findViewById(R.id.txt_nome);
+        txtFullName = findViewById(R.id.txt_fullname);
+
+        btnGetFullName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtFullName.setText(txtName.getText() + " " + txtLastName.getText());
+            }
+        });
+
+
     }
+
+
+
 }
